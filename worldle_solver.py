387e0bb73b71b0ac526e281fbef606568e9d3ff8
kenyrishi <kenyrishi@gmail.com>
2022-09-26 14:17:58 -0400

    add text file

--- worldle_solver.py
+++ worldle_solver.py
@@ -48,32 +48,32 @@ def find_best_word(green, yellow, white, possible_words):
             result = getClues(i,j)
             c2[j] = result
         clues[i] = c2
+    with open("word_score.txt", "a") as myfile:
+        for possible_best in possible_words:
+            print(possible_best)
+            average_removed_sum = 0
+            for possible_solution in possible_words:
+                total_removed = 0
+                new_g = green
+                new_y = list(yellow)
+                new_w = list(white)
+                result = clues[possible_best][possible_solution]
+                for i in range(5):
+                    if result[i] == "2":
+                        new_g[i] = possible_best[i]
+                    elif result[i] == "1":
+                        new_y.append(possible_best[i])
+                    elif result[i] == "0":
+                        new_w.append(possible_best[i])
 
-    for possible_best in possible_words:
-        print(possible_best)
-        average_removed_sum = 0
-        for possible_solution in possible_words:
-            total_removed = 0
-            new_g = green
-            new_y = list(yellow)
-            new_w = list(white)
-            result = clues[possible_best][possible_solution]
-            for i in range(5):
-                if result[i] == "2":
-                    new_g[i] = possible_best[i]
-                elif result[i] == "1":
-                    new_y.append(possible_best[i])
-                elif result[i] == "0":
-                    new_w.append(possible_best[i])
-
-            for w in possible_words:
-                if not isValid(w, new_g, new_y, new_w):
-                    total_removed += 1
-            average_removed_sum += total_removed/len(possible_words)
-        if best_word == "" or average_removed_sum > best_count:
-            best_word = possible_best
-            best_count = average_removed_sum
-        print(average_removed_sum/len(possible_words))
+                for w in possible_words:
+                    if not isValid(w, new_g, new_y, new_w):
+                        total_removed += 1
+                average_removed_sum += total_removed/len(possible_words)
+            if best_word == "" or average_removed_sum > best_count:
+                best_word = possible_best
+                best_count = average_removed_sum
+            myfile.write(possible_best + ": " + str(average_removed_sum/len(possible_words)))
     return best_word
 
     
